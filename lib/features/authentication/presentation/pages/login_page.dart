509978a0f538f1c15fbc33d7bd6c86e6cc5e746c
Nguyễn Quốc An <43922181+AnOnionz@users.noreply.github.com>
2024-02-29 22:46:28 +0700

    login page

--- lib/features/authentication/presentation/pages/login_page.dart
+++ lib/features/authentication/presentation/pages/login_page.dart
@@ -4,6 +4,7 @@ import 'package:fms/core/constant/images.dart';
 import 'package:fms/core/mixins/common.dart';
 import 'package:fms/core/mixins/fx.dart';
 import 'package:fms/core/responsive/responsive.dart';
+import 'package:fms/routes/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -11,71 +12,76 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppColors.aliceBlue,
         body: SafeArea(
-      child: Column(
-        children: [
-          Spacer(),
-          Image.asset(AppImages.logo),
-          Padding(
-            padding: EdgeInsets.only(top: 20.h, bottom: 16.h),
-            child: Text(context.language.appName,
-                style: context.textTheme.h1, textAlign: TextAlign.center),
-          ),
-          Text(
-            context.language.loginDescription,
-            style: context.textTheme.body1,
-            textAlign: TextAlign.center,
-          ),
-          Padding(
-            padding: Fx.pH20,
-            child: Image.asset(
-              AppImages.loginBanner,
-            ),
-          ),
-          const Spacer(),
-          Padding(
-            padding: Fx.pH32,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                    width: 106.sp,
-                    height: 1,
-                    decoration: BoxDecoration(color: AppColors.black)),
-                Text(context.language.loginText),
-                Container(
-                    width: 106.sp,
-                    height: 1,
-                    decoration: BoxDecoration(color: AppColors.black))
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 27.w, vertical: 26.h),
-            child: SizedBox(
-              width: context.screenSize.width,
-              child: TextButton(
-                onPressed: () {},
-                child: Padding(
-                  padding: Fx.pV16,
-                  child: Text(
-                    textScaler: TextScaler.linear(1),
-                    context.language.loginButtonText,
-                    style: context.textTheme.button2
-                        ?.copyWith(color: AppColors.white),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 90.h,
+              ),
+              Image.asset(AppImages.logo),
+              Padding(
+                padding: EdgeInsets.only(top: 20.h, bottom: 16.h),
+                child: Text(context.language.appName,
+                    style: context.textTheme.h1, textAlign: TextAlign.center),
+              ),
+              Text(
+                context.language.loginDescription,
+                style: context.textTheme.body1,
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: Fx.pH20,
+                child: Image.asset(
+                  AppImages.loginBanner,
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: Fx.pH32,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                        width: 106.sp,
+                        height: 1,
+                        decoration: BoxDecoration(color: AppColors.black)),
+                    Text(context.language.loginText),
+                    Container(
+                        width: 106.sp,
+                        height: 1,
+                        decoration: BoxDecoration(color: AppColors.black))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 27.w, vertical: 26.h),
+                child: SizedBox(
+                  width: context.screenSize.width,
+                  child: TextButton(
+                    onPressed: () {
+                      context.nextRoute(Routes.workForce);
+                    },
+                    child: Padding(
+                      padding: Fx.pV16,
+                      child: Text(
+                        textScaler: TextScaler.linear(1),
+                        context.language.loginButtonText,
+                        style: context.textTheme.button2
+                            ?.copyWith(color: AppColors.white),
+                      ),
+                    ),
+                    style: ButtonStyle(
+                        shape: MaterialStatePropertyAll<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50.dp))),
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(AppColors.orange)),
                   ),
                 ),
-                style: ButtonStyle(
-                    shape: MaterialStatePropertyAll<OutlinedBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50.dp))),
-                    backgroundColor:
-                        MaterialStatePropertyAll<Color>(AppColors.orange)),
-              ),
-            ),
-          )
-        ],
-      ),
-    ));
+              )
+            ],
+          ),
+        ));
   }
 }
