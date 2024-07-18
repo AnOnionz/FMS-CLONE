812bebdb02ccd6799bd79b599286aae051208abf
Nguyễn Quốc An <43922181+AnOnionz@users.noreply.github.com>
2024-07-18 11:49:05 +0700

    fixed login text

--- lib/features/work_place/presentation/pages/work_place_page.dart
+++ lib/features/work_place/presentation/pages/work_place_page.dart
@@ -98,7 +98,8 @@ class _WorkPlacePageState extends State<WorkPlacePage> with UserMixin {
         Padding(
           padding: Fx.pV16,
           child: Text(env.variables[Keys.APP_NAME] as String,
-              style: context.textTheme.h1, textAlign: TextAlign.center),
+              style: context.textTheme.h1?.copyWith(fontSize: 25.sp),
+              textAlign: TextAlign.center),
         ),
         // Padding(
         //   padding: EdgeInsets.symmetric(horizontal: 70.w),
